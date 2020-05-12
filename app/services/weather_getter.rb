9d4feec93314121eb3abd65569c0e187d44dffee
marc <marc.akbar@gmail.com>
2020-05-12 16:49:10 -0600

    Add geocoder and get weather by location

--- app/services/weather_getter.rb
+++ app/services/weather_getter.rb
@@ -1,5 +1,12 @@
 class WeatherGetter
 
+  def self.get_current_weather_by_location(location)
+    location = Geocoder.search(location)
+    coordinates = location.first.coordinates
+    
+    make_request("#{coordinates[0]},#{coordinates[1]}")
+  end
+
   def self.get_current_weather_by_lat_long(lat, long)
     make_request("#{lat},#{long}")
   end
--- spec/services/weather_getter_spec.rb
+++ spec/services/weather_getter_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 RSpec.describe WeatherGetter do
   describe "Send API Request" do
 
-    let(:lat)  { 40.014984 }
-    let(:long) { -105.270546 }
+    let(:lat)       { 40.014984 }
+    let(:long)      { -105.270546 }
+    let(:location)  { "Boulder" }
 
     it "returns weather day given lat and long" do
       response = WeatherGetter.get_current_weather_by_lat_long(lat, long)
+      expect(response["timezone"]).to eq "America/Denver"
+    end
+
+    it "returns weather day given location" do
+      response = WeatherGetter.get_current_weather_by_location(location)
       expect(response["timezone"]).to eq "America/Denver"
     end
 
