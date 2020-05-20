2cafd68d9a6760884f81cab59a8d656a44c02568
marc <marc.akbar@gmail.com>
2020-05-20 11:54:44 -0600

    Expand request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://marc-weather-back.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://marc-weather-back.herokuapp.com/search', 'https://marc-weather-back.herokuapp.com/search' ]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
